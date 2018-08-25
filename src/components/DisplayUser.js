d342248913d30e8f645a7ac58414d078a8e2fb63
Luis Dueñas <luis.alonso.duenas.m@gmail.com>
2018-08-25 11:24:33 -0700

    add deleted Username heading (lol!)

--- src/components/DisplayUser.js
+++ src/components/DisplayUser.js
@@ -13,6 +13,7 @@ const displayUser = ({ user, repos }) => {
 	const { login, avatar_url, name, bio, location, followers, following, isHireable } = user;
 	return (
 		<div className='DisplayUser'>
+			<h2>Username: {login}</h2>
 			<img className='avatar' src={avatar_url} alt='User profile avatar' />
 			<ValidatedField fieldName="Name" value={name}/>
 			<ValidatedField fieldName={"Bio: "} value={bio}/>
