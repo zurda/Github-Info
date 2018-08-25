2d4d708bbc0298693a7c34012a953f39eb323d19
Luis Dueñas <luis.alonso.duenas.m@gmail.com>
2018-08-25 11:22:46 -0700

    remove unnecessary comments

--- src/components/DisplayUser.js
+++ src/components/DisplayUser.js
@@ -13,17 +13,11 @@ const displayUser = ({ user, repos }) => {
 	const { login, avatar_url, name, bio, location, followers, following, isHireable } = user;
 	return (
 		<div className='DisplayUser'>
-			{/* <h2>Username: {login}</h2> */}
 			<img className='avatar' src={avatar_url} alt='User profile avatar' />
-			{/* <p>Name: {name}</p> */}
 			<ValidatedField fieldName="Name" value={name}/>
-			{/* <p>Bio: {bio}</p> */}
 			<ValidatedField fieldName={"Bio: "} value={bio}/>
-			{/* <p>Location: {location}</p> */}
 			<ValidatedField fieldName={"Location: "} value={location}/>
-			{/* <p>Followers: {followers}</p> */}
 			<ValidatedField fieldName={"Followers: "} value={followers}/>
-			{/* <p>Following: {following}</p> */}
 			<ValidatedField fieldName={"Following: "} value={following}/>
 			{isHireable && <p>Looking for a job</p>}
 		</div>
