425ef573834e8a95acf7886195071c1efc5a3f64
ksenousi <ksenousi@gmail.com>
2018-08-28 01:09:36 +0100

    format changes on GetInfo.js

--- src/components/GetInfo.js
+++ src/components/GetInfo.js
@@ -12,116 +12,116 @@ const sec = "1daa19d525b9e92f034ebe504075e5b1600eea46";
 const params = "?client_id=" + id + "&client_secret=" + sec;
 
 class GetInfo extends React.Component {
-    constructor (props) {
-        super(props);
-        this.state = { 
-            input: 'getify',
-            user: null,
-            repos: null,
-            isInvalid: false,
-            isFound: true,
-        }
-        this.getInfo = this.getInfo.bind(this);
-        this.inputHandler = this.inputHandler.bind(this);
-        this.keyDownHandler = this.keyDownHandler.bind(this);
-    }
+	constructor (props) {
+		super(props);
+		this.state = { 
+			input: 'getify',
+			user: null,
+			repos: null,
+			isInvalid: false,
+			isFound: true,
+		}
+		this.getInfo = this.getInfo.bind(this);
+		this.inputHandler = this.inputHandler.bind(this);
+		this.keyDownHandler = this.keyDownHandler.bind(this);
+	}
 
-    componentDidMount() {
-        this.getInfo();
-    }
+	componentDidMount() {
+		this.getInfo();
+	}
 
-    getInfo() {
-        const username = this.state.input;
-        // Check if the username is a valid one
-        if(!githubUsernameRegex.test(username)) {
-            // if not, set state to reflect so
-            // and exit the function without calling the API
-            this.setState({isInvalid: true})
-            return;
-        } else { this.setState({isInvalid: false})}
-        // Get user data
-        axios.get('https://api.github.com/users/' + username + params)
-            .then( 
-                // handle success
-                (response) => {
-                    this.setState({user: response.data})
-                    this.setState({isFound: true});
-                })
-                // handle error
-                .catch( (error) => {
-                    if(error.response.status === 404){
-                        this.setState({isFound: false});
-                    }
-                }
-            );
-        // Get repos data
-        axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100')
-            .then(
-                // handle success 
-                (response) => {
-                    this.setState({repos: response.data});
-                    console.log(response.data);
-                })
-                // handle error
-                .catch( (error) => { 
-                    console.log(error)
-                }
-            );
-        }
+	getInfo() {
+		const username = this.state.input;
+		// Check if the username is a valid one
+		if(!githubUsernameRegex.test(username)) {
+			// if not, set state to reflect so
+			// and exit the function without calling the API
+			this.setState({isInvalid: true})
+			return;
+		} else { this.setState({isInvalid: false})}
+		// Get user data
+		axios.get('https://api.github.com/users/' + username + params)
+			.then( 
+				// handle success
+				(response) => {
+					this.setState({user: response.data})
+					this.setState({isFound: true});
+				})
+				// handle error
+				.catch( (error) => {
+					if(error.response.status === 404){
+						this.setState({isFound: false});
+					}
+				}
+			);
+		// Get repos data
+		axios.get('https://api.github.com/users/' + username + '/repos' + params + '&per_page=100')
+			.then(
+				// handle success 
+				(response) => {
+					this.setState({repos: response.data});
+					console.log(response.data);
+				})
+				// handle error
+				.catch( (error) => { 
+					console.log(error)
+				}
+			);
+		}
 
-    inputHandler(event) {
-        const input = event.target.value;
-        this.setState({input});
-    }
+	inputHandler(event) {
+		const input = event.target.value;
+		this.setState({input});
+	}
 
-    keyDownHandler(event) {
-        if (event.keyCode === 13) {
-            document.getElementById('searchButton').click();
-        }
-    }
+	keyDownHandler(event) {
+		if (event.keyCode === 13) {
+			document.getElementById('searchButton').click();
+		}
+	}
 
-    render() {
-        const isInvalid = this.state.isInvalid;
-        const isFound = this.state.isFound;
-        let userDisplay;
-        let flashMessage;
-        if(isInvalid) {
-            // Show a message when the username is invalid
-            flashMessage =	<FlashMessage type="error">Invalid username</FlashMessage>;
-        } else if(!isFound){
-            // Show a message when the username is not found
-            flashMessage =	<FlashMessage type="error">Username not found</FlashMessage>;	
-        } else {
-            if(!(this.state.user || this.state.repos )){
-                flashMessage = <FlashMessage type="info">Loading...</FlashMessage>;
-            } else {
-                userDisplay = <div>
-                                <DisplayUser user={this.state.user} />
-                                <DisplayRepos repos={this.state.repos} />
-                            </div>
-            }
-        }
-            return (
-                <div>
-                    <div className='header'>
-                        <img className='logo' src={logo} alt='Github Profile Display Logo' />
-                        <h1 className='title' >Github Profiles</h1>
-                        <div className='userSearch'>
-                            <input className='searchInput' id='searchInputID'
-                                type="text" name="fname" placeholder="Search for a user"
-                            
-                                onChange={this.inputHandler}
-                                onKeyDown={this.keyDownHandler}
-                            />
-                            <button className='searchBtn' id='searchButton' onClick={this.getInfo}>Get info</button>
-                        </div>
-                        {flashMessage}
-                        {userDisplay}
-                       </div>
+	render() {
+		const isInvalid = this.state.isInvalid;
+		const isFound = this.state.isFound;
+		let userDisplay;
+		let flashMessage;
+		if(isInvalid) {
+			// Show a message when the username is invalid
+			flashMessage =	<FlashMessage type="error">Invalid username</FlashMessage>;
+		} else if(!isFound){
+			// Show a message when the username is not found
+			flashMessage =	<FlashMessage type="error">Username not found</FlashMessage>;	
+		} else {
+			if(!(this.state.user || this.state.repos )){
+				flashMessage = <FlashMessage type="info">Loading...</FlashMessage>;
+			} else {
+				userDisplay = <div>
+								<DisplayUser user={this.state.user} />
+								<DisplayRepos repos={this.state.repos} />
+							</div>
+			}
+		}
+			return (
+				<div>
+					<div className='header'>
+						<img className='logo' src={logo} alt='Github Profile Display Logo' />
+						<h1 className='title' >Github Profiles</h1>
+						<div className='userSearch'>
+							<input className='searchInput' id='searchInputID'
+								type="text" name="fname" placeholder="Search for a user"
+							
+								onChange={this.inputHandler}
+								onKeyDown={this.keyDownHandler}
+							/>
+							<button className='searchBtn' id='searchButton' onClick={this.getInfo}>Get info</button>
+						</div>
+						{flashMessage}
+						{userDisplay}
+					   </div>
 
-                </div>
-        );
-    }
+				</div>
+		);
+	}
 }
 
 export default GetInfo;
